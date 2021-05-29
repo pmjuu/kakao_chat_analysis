1a80c7dcf5714368258dcedf2208d7aba9427eac
Minju <50537876+pmjuu@users.noreply.github.com>
2021-05-29 14:01:04 +0900

    fix: 데이터 정제 - 메시지 내 줄바꿈 문제 미해결

--- chat_word_frequency.py
+++ chat_word_frequency.py
@@ -5,32 +5,32 @@ from pandas import DataFrame
 BASE_DIR = os.path.dirname(__file__)
 CHAT_DATA_DIR = os.path.join(BASE_DIR, 'chat_data')
 
-words = ['ㅎ']
+words = ['ㅎ', '번째']
 # words = input('공백을 구분자로 빈도를 알고 싶은 단어를 입력해주세요 : ').split()
 wordTable = {key:[] for key in words}
 
 with open(os.path.join(CHAT_DATA_DIR, "test.txt"), "r", encoding="utf-8") as f:
     lines = f.readlines()[2:]
     #데이터 정제하기
-    lines = [line.rstrip() for line in lines] #개행문자 삭제
-    lines = [line for line in lines if line != ''] #빈 줄 '' 삭제
+    lines = [line.rstrip() for line in lines if line != '\n'] #개행문자와 빈 줄 삭제
     for line in lines: #년월일 삭제
-        # line = line.rstrip() #왜 오류 나는거지??
         m = re.match('\d+년 \d+월 \d+일 \D요일', line)
         if m:
             print('년월일 삭제:', line)
             lines.remove(line)
 
     for line in lines: #줄바꿈된 문장 하나로 합치기
-        n = lines.index(line)
-        m = re.match('\d+. \d+. \d+.', line)
+        m = re.match('\d+. \d+. \d+. 오\D \d+:\d+, ', line)
         if not m:
-            # lines[n-1] = ' '.join(lines[(n-1):n]) #왜 안 되지??
-            lines[lines.index(line) - 1] += (' '+line)  # 이전 line에 현재 line 합치고
+            print('메시지 내 줄바꿈 감지:', line)
+            n = lines.index(line)
+            lines[n-1] += (' '+line)  #이전 line에 현재 line 합치고
             lines.remove(line) #현재 line 삭제
+        else:
+            pass
 
     print(lines)
-    with open(os.path.join(CHAT_DATA_DIR, "test_data_refine.txt"), "w", encoding="utf-8") as fw: #정제된 데이터
+    with open(os.path.join(CHAT_DATA_DIR, "test_data_refine.txt"), "w", encoding="utf-8") as fw: #정제된 데이터txt파일
         for line in lines:
             fw.write(line+'\n')
 
