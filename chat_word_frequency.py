1b290da6ff977c91560b88536ab407fc9ce00094
Minju <50537876+pmjuu@users.noreply.github.com>
2021-05-30 22:09:38 +0900

    fix: 알고리즘 수정

--- chat_word_frequency.py
+++ chat_word_frequency.py
@@ -5,34 +5,36 @@ from pandas import DataFrame
 BASE_DIR = os.path.dirname(__file__)
 CHAT_DATA_DIR = os.path.join(BASE_DIR, 'chat_data')
 
-words = ['ㅎ', '사용자', ' : ']
-# words = input('공백을 구분자로 빈도를 알고 싶은 단어를 입력해주세요 : ').split()
+# words = ['ㅎ', '사용자', ' : ']
+words = input('공백을 구분자로 빈도를 알고 싶은 단어를 입력해주세요 : ').split()
 wordTable = {key:[] for key in words}
 
-with open(os.path.join(CHAT_DATA_DIR, "test.txt"), "r", encoding="utf-8") as f:
+with open(os.path.join(CHAT_DATA_DIR, "2021.05.20.txt"), "r", encoding="utf-8") as f:
     '''데이터 정제하기'''
-    lines = f.read()
-    lines = lines.replace('\n', ' ')
-    lines = re.sub('\d+년 \d+월 \d+일 \D요일', '', lines)
+    data = f.read()
+    data = re.sub('\d+년 \d+월 \d+일 \D요일\n', '', data)
+    data = data.replace('\n', ' ')
 
-    date_time = re.findall('\d+[.] \d+[.] \d+[.] 오\D \d+:\d+, ', lines) #날짜시각 추출
-    date_time = sorted(set(date_time))
+    sent_time = re.findall('\d+[.] \d+[.] \d+[.] 오\D \d+:\d+, ', data) #날짜시각 추출
 
-    lines = [lines]
-    for i in date_time:
-        temp = lines[-1]
-        lines.pop()
-        lines.extend(temp.split(i))
-    lines = lines[1:]
+    data = [data]
+    for i in sent_time:
+        temp = data[-1]
+        data.pop()
+        data.extend(temp.split(i))
+    data = data[1:]
 
     '''정제된 데이터txt파일'''
-    with open(os.path.join(CHAT_DATA_DIR, "test_data_refine.txt"), "w", encoding="utf-8") as fw:
-        for line in lines:
+    with open(os.path.join(CHAT_DATA_DIR, "refined_data.txt"), "w", encoding="utf-8") as fw:
+        for line in data:
+            fw.write(line+'\n')
+    with open(os.path.join(CHAT_DATA_DIR, "sent_time.txt"), "w", encoding="utf-8") as fw:
+        for line in sent_time:
             fw.write(line+'\n')
 
-    names = sorted({line.split(' : ')[0] for line in lines}) #사용자 이름 추출
+    names = sorted({line.split(' : ')[0] for line in data}) #사용자 이름 추출
 
-    user_linesList = [[line.split(name+' : ')[1] for line in lines if (name+' : ') in line] for name in names] #사용자 별로 문장 분류. ex)첫번째 list는 사용자1의 문장들
+    user_linesList = [[line.split(name+' : ')[1] for line in data if (name+' : ') in line] for name in names] #사용자 별로 문장 분류. ex)첫번째 list는 사용자1의 문장들
 
     for user_lines in user_linesList: #ex)사용자1 문장들 부터 분석 시작
         for word in words:
